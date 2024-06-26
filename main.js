5b7547df5014a4d4241fb7d9689bfd6a75845eb0
TimmPeterson <156585171+TimmPeterson@users.noreply.github.com>
2024-06-26 17:15:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 import http from "node:http";
 import fs from "node:fs/promises"; // for working with files
-import { WebSocketServer } from "ws";
-import express from "express";
-import { MongoClient, ObjectId } from "mongodb";
-import morgan from "morgan";
+import { WebSocketServer } from "ws"
+import express from "express"
+import { MongoClient, ObjectId } from "mongodb"
 
 /////
 // Mongo initializing
 /////
 
 const url =
-  "mongodb+srv://doadmin:x62jNC54Pi1W3t98@db-mongodb-pml30-2024-12312526.mongo.ondigitalocean.com/admin?tls=true&authSource=admin";
+ "mongodb+srv://doadmin:x62jNC54Pi1W3t98@db-mongodb-pml30-2024-12312526.mongo.ondigitalocean.com/admin?tls=true&authSource=admin";
 const Client = new MongoClient(url);
 const connection = await Client.connect();
 const dataBase = "PML30-2024-J";
 const db = connection.db(dataBase);
 const collection = db.collection("TP5");
-
 /////
 // End of mongo initialiation
 /////
@@ -26,7 +24,7 @@ let messages = [];
 
 // Array with all users
 let users = {
-  user: { partner: [] },
+    "user": { "partner": [] }
 };
 
 // Array with current pool of sockets (clients)
@@ -40,13 +38,12 @@ const app = express();
 // For debug and for example
 let counter = 0;
 app.get("/", (req, res, next) => {
-  counter = counter + 1;
-  console.log(counter);
-  next();
+    counter = counter + 1;
+    console.log(counter);
+    next();
 });
 
 app.use(express.static("client"));
-app.use(morgan("tiny"));
 
 // Creating http server
 const server = http.createServer(app);
@@ -56,79 +53,84 @@ const wss = new WebSocketServer({ server });
 
 // Event on server socket connecting to client socket
 wss.on("connection", (ws) => {
-  // Event on getting message on server from client socket
-  ws.on("message", async (message) => {
-    // Message should be an array of symbols.
-    // We can convert it to string using msg.toString().
-    // And then we can get an object from it using JSON.parse(msg) .
-    // We should always send a string.
-    // So we use JSON.stringify(msg) to convert object o string
-    let msg = JSON.parse(message.toString());
+    // Event on getting message on server from client socket
+    ws.on("message", async (message) => {
+        // Message should be an array of symbols.
+        // We can convert it to string using msg.toString().
+        // And then we can get an object from it using JSON.parse(msg) .
+        // We should always send a string.
+        // So we use JSON.stringify(msg) to convert object o string 
+        let msg = JSON.parse(message.toString())
 
-    if (msg.get_global != undefined) {
-      ws.send(JSON.stringify(messages));
-    } else if (msg.send_global != undefined) {
-      messages.push(msg);
-      ws.send(JSON.stringify(messages));
-    } else {
-      if (msg.get != undefined) {
-        // if (users[msg.user] == undefined || users[msg.user][msg.partner] == undefined)
-        //     ws.send(JSON.stringify([]));
-        // else
-        //     ws.send(JSON.stringify(users[msg.user][msg.partner]));
+        if (msg.get_global != undefined) {
+            ws.send(JSON.stringify(messages));
+        } else if (msg.send_global != undefined) {
+            messages.push(msg);
+            ws.send(JSON.stringify(messages));
+        } else {
+            if (msg.get != undefined) {
+                // if (users[msg.user] == undefined || users[msg.user][msg.partner] == undefined)
+                //     ws.send(JSON.stringify([]));
+                // else
+                //     ws.send(JSON.stringify(users[msg.user][msg.partner]));
 
-        let n = await collection.countDocuments({ user: msg.user });
-        if (n === 0) {
-          ws.send(JSON.stringify([]));
-        } else {
-          for await (let js of collection.find({ user: msg.user })) {
-            if (js.chats[msg.partner] == undefined) ws.send(JSON.stringify([]));
-            else ws.send(JSON.stringify(js.chats[msg.partner]));
-          }
-        }
-      } else {
-        // if (users[msg.user] == undefined)
-        //     users[msg.user] = {};
-        // if (users[msg.partner] == undefined)
-        //     users[msg.partner] = {};
-        // if (users[msg.user][msg.partner] == undefined)
-        //     users[msg.user][msg.partner] = [];
-        // if (users[msg.partner][msg.user] == undefined)
-        //     users[msg.partner][msg.user] = [];
-        // users[msg.user][msg.partner].push(msg);
-        // users[msg.partner][msg.user].push(msg);
+                let n = await collection.countDocuments({ user: msg.user });
+                if (n === 0) {
+                    ws.send(JSON.stringify([]));
+                } else {
+                    for await (let js of collection.find({ user: msg.user })) {
+                        if (js.chats[msg.partner] == undefined)
+                            ws.send(JSON.stringify([]));
+                        else
+                            ws.send(JSON.stringify(js.chats[msg.partner]));
+                    }
+                }
+            } else {
+                // if (users[msg.user] == undefined)
+                //     users[msg.user] = {};
+                // if (users[msg.partner] == undefined)
+                //     users[msg.partner] = {};
+                // if (users[msg.user][msg.partner] == undefined)
+                //     users[msg.user][msg.partner] = [];
+                // if (users[msg.partner][msg.user] == undefined)
+                //     users[msg.partner][msg.user] = [];
+                // users[msg.user][msg.partner].push(msg);
+                // users[msg.partner][msg.user].push(msg);
 
-        let js = { user: msg.user };
-        let n = await collection.countDocuments(js);
-        if (n === 0) {
-          js.chats = {};
-          js.chats[msg.partner] = [msg];
-          await collection.insertOne(js);
-        } else {
-          for await (let js1 of collection.find(js)) {
-            if (js1.chats[msg.partner] == undefined)
-              js1.chats[msg.partner] = [msg];
-            else js1.chats[msg.partner].push(msg);
-            await collection.replaceOne(js, js1);
-          }
+                let js = { user: msg.user };
+                let n = await collection.countDocuments(js);
+                if (n === 0) {
+                    js.chats = {};
+                    js.chats[msg.partner] = [msg];
+                    await collection.insertOne(js);
+                } else {
+                    for await (let js1 of collection.find(js)) {
+                        if (js1.chats[msg.partner] == undefined)
+                            js1.chats[msg.partner] = [msg];
+                        else
+                            js1.chats[msg.partner].push(msg);
+                        await collection.replaceOne(js, js1);
+                    }
+                }
+                js = { user: msg.partner };
+                n = await collection.countDocuments(js);
+                if (n === 0) {
+                    js.chats = {};
+                    js.chats[msg.user] = [msg];
+                    await collection.insertOne(js);
+                } else {
+                    for await (let js1 of collection.find(js)) {
+                        if (js1.chats[msg.user] == undefined)
+                            js1.chats[msg.user] = [msg];
+                        else
+                            js1.chats[msg.user].push(msg);
+                        await collection.replaceOne(js, js1);
+                    }
+                }
+                //ws.send(JSON.stringify(users[msg.user][msg.partner]));
+            }
         }
-        js = { user: msg.partner };
-        n = await collection.countDocuments(js);
-        if (n === 0) {
-          js.chats = {};
-          js.chats[msg.user] = [msg];
-          await collection.insertOne(js);
-        } else {
-          for await (let js1 of collection.find(js)) {
-            if (js1.chats[msg.user] == undefined) js1.chats[msg.user] = [msg];
-            else js1.chats[msg.user].push(msg);
-            await collection.replaceOne(js, js1);
-          }
-        }
-        //ws.send(JSON.stringify(users[msg.user][msg.partner]));
-      }
-    }
-    /*
+        /*
         if (msg.client_start != undefined) {
             ws.send(JSON.stringify(messages));
         } else {
@@ -137,29 +139,23 @@ wss.on("connection", (ws) => {
                 sck.send(JSON.stringify(messages));
         }
         */
-  });
+    });
 
-  // Removing socket from pool of sockets with callback on closing event
-  ws.on("close", () => {
-    sockets.slice(sockets.indexOf(ws), 1);
-  });
+    // Removing socket from pool of sockets with callback on closing event 
+    ws.on("close", () => {
+        sockets.slice(sockets.indexOf(ws), 1);
+    });
 
-  // Pushing current client socket to the pool of sockets on connection event
-  sockets.push(ws);
+    // Pushing current client socket to the pool of sockets on connection event 
+    sockets.push(ws);
 });
 
 // Some stuff for debug
-// const host = "";
-// const port = 8000;
-// server.listen(port, host, () => {
-//   console.log(`server started on http://${host}:${port}`);
-//});
-
-// Some stuff for debug
+const host = "localhost";
 const port = 8000;
-server.listen(port, () => {
-  console.log(`server started on http://:${port}`);
+server.listen(port, host, () => {
+    console.log(`server started on http://${host}:${port}`);
 });
 
-const main = () => {};
+const main = () => { }
 main();
